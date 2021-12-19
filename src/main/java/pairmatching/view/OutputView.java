b82f693901fd9d594470da1afb0c90c366be4138
HyeonbinSa <shb1833@gmail.com>
2021-12-19 23:26:31 +0900

    refactor: OutputView 문자열 상수화

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -6,29 +6,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
-    private static final String DIVIDER = "#############################################";
+    private static final String DIVIDER_LINE = "#############################################";
+    private static final String PAIR_MATCH_RESULT = "페어 매칭 결과입니다.";
+    private static final String NEW_LINE = "\n";
     private static final String ERROR = "[ERROR] ";
+    private static final String COURSE_PREFIX = "과정: ";
+    private static final String MISSION_PREFIX = "미션: ";
+    private static final String MISSION_SUFFIX = ": ";
+    private static final String LEVEL_PREFIX = "- ";
+    private static final String DIVIDER = " | ";
+    private static final String PAIR_DIVIDER = " : ";
 
     public void printInformation() {
-        System.out.println(DIVIDER);
+        System.out.println(NEW_LINE + DIVIDER_LINE);
         printCourse();
         printMissions();
-        System.out.println(DIVIDER);
+        System.out.println(DIVIDER_LINE);
     }
 
     private void printCourse() {
-        System.out.print("과정: ");
+        System.out.print(COURSE_PREFIX);
         List<String> stringList = new ArrayList<>();
         for (Course course : Course.values()) {
             stringList.add(course.getName());
         }
-        System.out.println(String.join(" | ", stringList));
+        System.out.println(String.join(DIVIDER, stringList));
     }
 
     private void printMissions() {
-        System.out.println("미션: ");
+        System.out.println(MISSION_PREFIX);
         for (Level level : Level.values()) {
-            System.out.print("- " + level.getName() + ": ");
+            System.out.print(LEVEL_PREFIX + level.getName() + MISSION_SUFFIX);
             printMission(level.getMissions());
         }
     }
@@ -38,11 +46,11 @@ public class OutputView {
         for (Mission mission : missions) {
             stringList.add(mission.getName());
         }
-        System.out.println(String.join(" | ", stringList));
+        System.out.println(String.join(DIVIDER, stringList));
     }
 
     public void printPairList(Course course, Mission mission) {
-        System.out.println("페어 매칭 결과입니다.");
+        System.out.println(PAIR_MATCH_RESULT);
         PairList pairList = mission.getPairList(course);
         for (Pair pair : pairList.getPairList()) {
             printPair(pair);
@@ -50,7 +58,7 @@ public class OutputView {
     }
 
     public void printPair(Pair pair) {
-        System.out.println(String.join(" : ", pair.getPair()));
+        System.out.println(String.join(PAIR_DIVIDER, pair.getPair()));
     }
 
     public void printError(String message) {
